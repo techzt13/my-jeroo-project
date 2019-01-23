18ae9d1941bd03ffff3d1754c958d951723d6f0a
Ben Konz <bkonz@unomaha.edu>
2019-01-22 18:16:12 -0600

    Fixed AST constructors to be usable in menhir

--- src/compiler/lib/AST.ml
+++ src/compiler/lib/AST.ml
@@ -1,34 +1,45 @@
-type expr =
-  | IdExpr of string
-  | IntExpr of int
-  | TrueExpr
-  | FalseExpr
-  | LeftExpr
-  | RightExpr
-  | AheadExpr
-  | HereExpr
-  | NorthExpr
-  | EastExpr
-  | SourthExpr
-  | WestExpr
-  | BinOpExpr of expr * string * expr
-  | UnOpExpr of string * expr
-  (* method, optional argument list *)
-  | FxnAppExpr of string * expr list option
-  (* object, method, optional arguments list *)
-  | ObjFxnAppExpr of string * string * expr option list option
+type bin_op = [
+  | `And
+  | `Or
+]
 
-(* type, identifier, constructor, optional arguments list *)
-type decl = string * string * string * expr list option
+type un_op = [
+  | `Not
+]
 
-type stmt =
-  | ExprStmt of expr
+type expr = [
+  | `IdExpr of string
+  | `IntExpr of int
+  | `TrueExpr
+  | `FalseExpr
+  | `LeftExpr
+  | `RightExpr
+  | `AheadExpr
+  | `HereExpr
+  | `NorthExpr
+  | `EastExpr
+  | `SouthExpr
+  | `WestExpr
+  | `BinOpExpr of expr * bin_op * expr
+  | `UnOpExpr of un_op * expr
+  (* method, optional list *)
+  | `FxnAppExpr of string * expr list
+  (* object, method, arguments list *)
+  | `ObjFxnAppExpr of string * string * expr list
+]
+
+(* type, identifier, constructor, arguments list *)
+type decl = string * string * string * expr list
+
+type stmt = [
+  | `ExprStmt of expr
   (* condition, positive body *)
-  | IfStmt of expr * decl list option * stmt list option
+  | `IfStmt of expr * (decl list * stmt list)
   (* condition, positive branch body, negative branch body *)
-  | IfElseStmt of expr * (decl list option * stmt list option) * (decl list option * stmt list option)
+  | `IfElseStmt of expr * (decl list * stmt list) * (decl list * stmt list)
   (* loop condition, loop body *)
-  | WhileStmt of expr * decl list option * stmt list option
+  | `WhileStmt of expr * (decl list * stmt list)
+]
 
 (* function name, function body *)
-type fxn = string * decl list option * stmt list option
+type fxn = string * (decl list * stmt list)
