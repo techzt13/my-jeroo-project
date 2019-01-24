fddd70b4dc1db9010494f86152bdd43bb7a1f5d6
Ben Konz <bkonz@unomaha.edu>
2019-01-23 23:22:03 -0600

    Added Java parsing tests

--- src/compiler/test/test.ml
+++ src/compiler/test/test.ml
@@ -1,10 +1,101 @@
 open OUnit2
+open Lib
 
-let test1 _test_ctxt = assert_equal "x" "x"
+let parse_string s =
+  let lexbuf = Lexing.from_string s in
+  Lib.Parser.translation_unit Lexer.token lexbuf
+
+let parse_method _test_ctxt =
+  let code = "method main() { }" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], []))]
+
+let parse_decl _test_ctxt =
+  let code = "method main() { Jeroo j = new Jeroo(1, 2); }" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([
+      ("Jeroo", "j", `UnOpExpr(`New, `FxnAppExpr(`IdExpr("Jeroo"), [`IntExpr(1); `IntExpr(2)])))
+    ], []))]
+
+let parse_if_stmt _test_ctxt =
+  let code = "method main() { if (true) { } }" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `IfStmt(`TrueExpr, `BlockStmt([], []))]))]
+
+let parse_if_else_stmt _test_ctxt =
+  let code = "method main() { if (true) { } else { }}" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `IfElseStmt(`TrueExpr, `BlockStmt([], []), `BlockStmt([], []))
+    ]))]
+
+let parse_while_stmt _test_ctxt =
+  let code = "method main() { while(true) { }}" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `WhileStmt(`TrueExpr, `BlockStmt([], []))
+    ]))]
+
+let parse_and _test_ctxt =
+  let code = "method main() { if (true && true) { }}" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `IfStmt(`BinOpExpr(`TrueExpr, `And, `TrueExpr), `BlockStmt([], []))
+    ]))]
+
+let parse_or _test_ctxt =
+  let code = "method main() { if (true || true) { }}" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `IfStmt(`BinOpExpr(`TrueExpr, `Or, `TrueExpr), `BlockStmt([], []))
+    ]))]
+
+let parse_not _test_ctxt =
+  let code = "method main() { if (!true) {} }" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `IfStmt(`UnOpExpr(`Not, `TrueExpr), `BlockStmt([], []))
+    ]))]
+
+let parse_not_precedence _test_ctxt =
+  let code = "method main() { if (!true && false) {}}" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `IfStmt(`BinOpExpr(`UnOpExpr(`Not, `TrueExpr), `And, `FalseExpr), `BlockStmt([], []))
+    ]))]
+
+let parse_comment _test_ctxt =
+  let code = "// this is a comment\n method main() { }" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], []))]
+
+let parse_ml_comment _test_ctxt =
+  let code = "/* this is a *\n\n\n* multi line comment */method main() {}" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], []))]
+
+let parse_obj_call _test_ctxt =
+  let code = "method main() { j.someFxn(1, NORTH);  }" in
+  let ast = parse_string code in
+  assert_equal ast [("main", ([], [
+      `ExprStmt(`BinOpExpr(`IdExpr("j"), `Dot, `FxnAppExpr(`IdExpr("someFxn"), [`IntExpr(1); `NorthExpr])))
+    ]))]
 
 let suite =
-  "suite">::: [
-    "test1">:: test1;
+  "Java Parsing">::: [
+    "Parse Method">:: parse_method;
+    "Parse Decl">:: parse_decl;
+    "Parse If Stmt">:: parse_if_stmt;
+    "Parse If Else Stmt">:: parse_if_else_stmt;
+    "Parse While Stmt">:: parse_while_stmt;
+    "Parse And">:: parse_and;
+    "Parse Or">:: parse_or;
+    "Parse Not">:: parse_not;
+    "Parse Not Precedence">:: parse_not_precedence;
+    "Parse comments">:: parse_comment;
+    "Parse ml-comment">:: parse_ml_comment;
+    "Parse obj call">:: parse_obj_call;
   ]
 
 let () =
