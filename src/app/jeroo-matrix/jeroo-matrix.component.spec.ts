d0ac2a4eeeac895dc5450c3cb1157f6d84408697
cpbryan <cpbryan@unomaha.edu>
2019-01-25 20:53:23 -0600

    Merge branch 'master' into jeroo-matrix

--- src/app/jeroo-matrix/jeroo-matrix.component.spec.ts
+++ src/app/jeroo-matrix/jeroo-matrix.component.spec.ts
@@ -2,12 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JerooMatrixComponent } from './jeroo-matrix.component';
 
+import { MaterialModule } from '../material.module';
+
 describe('JerooMatrixComponent', () => {
   let component: JerooMatrixComponent;
   let fixture: ComponentFixture<JerooMatrixComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MaterialModule
+      ],
       declarations: [ JerooMatrixComponent ]
     })
     .compileComponents();
