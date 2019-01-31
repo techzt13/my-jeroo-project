c550979bc3430f80d0ef8340fda5cdb91be343a9
Ben Konz <bkonz@unomaha.edu>
2019-01-31 00:00:49 -0600

    fixed linting error

--- src/app/help/general/help-action/help-action.component.ts
+++ src/app/help/general/help-action/help-action.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 @Component({
-  selector: 'app-help-action',
-  templateUrl: './help-action.component.html',
-  styleUrls: ['./help-action.component.scss']
+    selector: 'app-help-action',
+    templateUrl: './help-action.component.html',
+    styleUrls: ['./help-action.component.scss']
 })
 export class HelpActionComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-  
+    ngOnInit() {
+    }
 }
 
