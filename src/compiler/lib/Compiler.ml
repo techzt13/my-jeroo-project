138f49c600c01e7823da751348fbe12db2b746fc
Ben Konz <bkonz@unomaha.edu>
2019-02-10 14:07:15 -0600

    Removed Str dependency since js_of_ocaml doesn't have an impl

--- src/compiler/lib/Compiler.ml
+++ src/compiler/lib/Compiler.ml
@@ -3,19 +3,10 @@ exception HeaderException of string
 let compile code =
   let lexbuf = Lexing.from_string code in
 
-  let regex = Str.regexp "^@\\(Java\\|VB\\)$" in
-  let _ =
-    try Str.search_forward regex code 0
-    with Not_found -> raise (HeaderException "Malformed Header")
+  (* get the first line from the code and pair the header to the correct language *)
+  let ast = match (String.split_on_char '\n' code) with
+    | "@Java" :: _ -> JavaParser.translation_unit JavaLexer.token lexbuf
+    | "@VB" :: _ -> VBParser.translation_unit VBLexer.token lexbuf
+    | _ -> raise (HeaderException "Malformed Header")
   in
-  let language_id =
-    try Str.matched_group 1 code
-    with Not_found -> raise (HeaderException "Missing Language ID in Header")
-  in
-  let ast = match language_id with
-    | "Java" -> JavaParser.translation_unit JavaLexer.token lexbuf
-    | "VB" -> VBParser.translation_unit VBLexer.token lexbuf
-    | _ -> raise (HeaderException "Unknown Language")
-  in
-
   Codegen.codegen ast
