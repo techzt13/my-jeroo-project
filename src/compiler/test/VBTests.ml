667a1c311c0c0f446aa30fe60b3c3cf092b91951
Ben Konz <bkonz@unomaha.edu>
2019-02-19 21:00:38 -0600

    Fixed VB tests

--- src/compiler/test/VBTests.ml
+++ src/compiler/test/VBTests.ml
@@ -1,5 +1,6 @@
 open OUnit2
 open Lib
+open AST
 
 let parse_string s =
   let lexbuf = Lexing.from_string s in
@@ -8,243 +9,540 @@ let parse_string s =
 let parse_method _test_ctxt =
   let code = "@VB\n @@\n sub main() \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", []);
+    main_fxn = {
+      id = "main";
+      stmts = [];
+      start_lnum = 0;
+      end_lnum = 1;
+    }
   } in
   assert_equal ast expected
 
 let parse_decl _test_ctxt =
   let code = "@VB\n @@\n sub main() \n dim j as jeroo = new jeroo(1, 2) \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `DeclStmt("Jeroo", "j", `UnOpExpr(`New, `FxnAppExpr(`IdExpr("Jeroo"), [`IntExpr(1); `IntExpr(2)])))
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `DeclStmt("Jeroo", "j", {
+            a = `UnOpExpr(`New, {
+                a = `FxnAppExpr({
+                    a = `IdExpr("Jeroo");
+                    lnum = 1;
+                  }, [
+                      {
+                        a = `IntExpr(1);
+                        lnum = 1;
+                      };
+                      {
+                        a = `IntExpr(2);
+                        lnum = 1;
+                      };
+                    ]);
+                lnum = 1;
+              });
+            lnum = 1;
+          })
+      ];
+      start_lnum = 0;
+      end_lnum = 2;
+    }
   } in
   assert_equal ast expected
 
 let parse_if_stmt _test_ctxt =
   let code = "@VB\n @@\n sub main() \n if (true) then \n end if \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`TrueExpr, `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_if_stmt_no_paren _test_ctxt =
   let code = "@VB\n @@\n sub main() \n if true then \n end if \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`TrueExpr, `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_elseif_stmt _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if (true) then\n elseif (false) then\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfElseStmt(`TrueExpr, `BlockStmt [], `IfStmt(`FalseExpr, `BlockStmt []))
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfElseStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], `IfStmt({
+            a = `FalseExpr;
+            lnum = 2;
+          }, `BlockStmt [], 2), 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 4;
+    }
   } in
   assert_equal ast expected
 
 let parse_elseif_no_paren_stmt _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if true then\n elseif false then\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfElseStmt(`TrueExpr, `BlockStmt [], `IfStmt(`FalseExpr, `BlockStmt []))
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfElseStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], `IfStmt({
+            a = `FalseExpr;
+            lnum = 2;
+          }, `BlockStmt [], 2), 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 4;
+    }
   } in
   assert_equal ast expected
 
 let parse_elseif_list_stmt _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if (true) then\n elseif (false) then\n elseif (true) then\n else\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfElseStmt(`TrueExpr, `BlockStmt [], `IfElseStmt(`FalseExpr, `BlockStmt [], `IfElseStmt(`TrueExpr, `BlockStmt [], `BlockStmt [])))
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfElseStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], `IfElseStmt({
+            a = `FalseExpr;
+            lnum = 2;
+          }, `BlockStmt [], `IfElseStmt({
+            a = `TrueExpr;
+            lnum = 3;
+          }, `BlockStmt [], `BlockStmt [], 3), 2), 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 6;
+    }
   } in
   assert_equal ast expected
 
 let parse_elseif_list_no_paren_stmt _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if true then\n elseif false then\n elseif true then\n else\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfElseStmt(`TrueExpr, `BlockStmt [], `IfElseStmt(`FalseExpr, `BlockStmt [], `IfElseStmt(`TrueExpr, `BlockStmt [], `BlockStmt [])))
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfElseStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], `IfElseStmt({
+            a = `FalseExpr;
+            lnum = 2;
+          }, `BlockStmt [], `IfElseStmt({
+            a = `TrueExpr;
+            lnum = 3;
+          }, `BlockStmt [], `BlockStmt [], 3), 2), 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 6;
+    }
   } in
   assert_equal ast expected
 
 let parse_while_stmt _test_ctxt =
   let code = "@VB\n @@\n sub main() \n while (true) \n end while \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `WhileStmt(`TrueExpr, `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `WhileStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_while_no_paren _test_ctxt =
   let code = "@VB\n @@\n sub main() \n while true \n end while \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `WhileStmt(`TrueExpr, `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `WhileStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_and _test_ctxt =
   let code = "@VB\n @@\n sub main() \n if true and true then \n end if \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`BinOpExpr(`TrueExpr, `And, `TrueExpr), `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `BinOpExpr({
+                a = `TrueExpr;
+                lnum = 1;
+              }, `And, {
+                  a = `TrueExpr;
+                  lnum = 1;
+                });
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_or _test_ctxt =
   let code = "@VB\n @@\n sub main() \n if true or true then \n end if \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`BinOpExpr(`TrueExpr, `Or, `TrueExpr), `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `BinOpExpr({
+                a = `TrueExpr;
+                lnum = 1;
+              }, `Or, {
+                  a = `TrueExpr;
+                  lnum = 1;
+                });
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_not _test_ctxt =
   let code = "@VB\n @@\n sub main() \n if not false then \n end if \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`UnOpExpr(`Not, `FalseExpr), `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `UnOpExpr(`Not, {
+                a = `FalseExpr;
+                lnum = 1;
+              });
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_comment _test_ctxt =
-  let code = "'asdfasdf \n @VB\n @@\n sub main() \n end sub" in
+  let code = "@VB\n @@\n 'asdfasdf \n sub main() \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", []);
+    main_fxn = {
+      id = "main";
+      stmts = [];
+      start_lnum = 1;
+      end_lnum = 2;
+    }
   } in
   assert_equal ast expected
 
 let parse_fxn_call _test_ctxt =
   let code = "@VB\n @@\n sub main() \n foobar(20, five, north) \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `ExprStmt(`FxnAppExpr(`IdExpr("foobar"), [`IntExpr(20); `IdExpr("five"); `NorthExpr])
-                 )]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `ExprStmt({
+            a = `FxnAppExpr({
+                a = `IdExpr("foobar");
+                lnum = 1;
+              }, [
+                  {
+                    a = `IntExpr(20);
+                    lnum = 1;
+                  };
+                  {
+                    a = `IdExpr("five");
+                    lnum = 1;
+                  };
+                  {
+                    a = `NorthExpr;
+                    lnum = 1;
+                  }
+                ]);
+            lnum = 1;
+          })
+      ];
+      start_lnum = 0;
+      end_lnum = 2;
+    }
   } in
   assert_equal ast expected
 
 let parse_obj_fxn_call _test_ctxt =
   let code = "@VB\n @@\n sub main() \n foo.bar(AHEAD) \n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `ExprStmt(`BinOpExpr(`IdExpr("foo"), `Dot, `FxnAppExpr(`IdExpr("bar"), [`AheadExpr])))
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `ExprStmt({
+            a = `BinOpExpr({
+                a = `IdExpr("foo");
+                lnum = 1;
+              }, `Dot, {
+                  a = `FxnAppExpr({
+                      a = `IdExpr("bar");
+                      lnum = 1;
+                    }, [{
+                      a = `AheadExpr;
+                      lnum = 1;
+                    }]);
+                  lnum = 1
+                });
+            lnum = 1;
+          })
+      ];
+      start_lnum = 0;
+      end_lnum = 2;
+    }
   } in
   assert_equal ast expected
 
 let parse_not_precedence _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if true and not false then\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`BinOpExpr(`TrueExpr, `And, `UnOpExpr(`Not, `FalseExpr)), `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `BinOpExpr({
+                a = `TrueExpr;
+                lnum = 1;
+              }, `And, {
+                  a = `UnOpExpr(`Not, {
+                      a = `FalseExpr;
+                      lnum = 1;
+                    });
+                  lnum = 1;
+                });
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_paren_precedence _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if not (true and false) then\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`UnOpExpr(`Not, `BinOpExpr(`TrueExpr, `And, `FalseExpr)), `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `UnOpExpr(`Not, {
+                a = `BinOpExpr({
+                    a = `TrueExpr;
+                    lnum = 1;
+                  }, `And, {
+                      a = `FalseExpr;
+                      lnum = 1;
+                    });
+                lnum = 1;
+              });
+            lnum = 1
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_and_or_precedence _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if true or false and true then\n end if\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [
-        `IfStmt(`BinOpExpr(`TrueExpr, `Or, `BinOpExpr(`FalseExpr, `And, `TrueExpr)), `BlockStmt [])
-      ]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `BinOpExpr({
+                a = `TrueExpr;
+                lnum = 1;
+              }, `Or, {
+                  a = `BinOpExpr({
+                      a = `FalseExpr;
+                      lnum = 1;
+                    }, `And, {
+                        a = `TrueExpr;
+                        lnum = 1;
+                      });
+                  lnum = 1;
+                });
+            lnum = 1;
+          }, `BlockStmt [], 1)
+      ];
+      start_lnum = 0;
+      end_lnum = 3;
+    }
   } in
   assert_equal ast expected
 
 let parse_arbitrary_newlines _test_ctxt =
   let code = "@VB\n\n @@\n\n\n sub main()\n\n\n\n end sub\n   \n   \n" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", []);
+    main_fxn = {
+      id = "main";
+      stmts = [];
+      start_lnum = 2;
+      end_lnum = 6;
+    }
   } in
   assert_equal ast expected
 
 let parse_fxn_list _test_ctxt =
   let code = "@VB\n sub foo()\n end sub\n @@\n sub main()\n end sub\n\n\n" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
-    extension_fxns = [("foo", [])];
-    main_fxn = ("main", []);
+  let expected = {
+    extension_fxns = [
+      {
+        id = "foo";
+        stmts = [];
+        start_lnum = 1;
+        end_lnum = 2;
+      }
+    ];
+    main_fxn = {
+      id = "main";
+      stmts = [];
+      start_lnum = 0;
+      end_lnum = 1;
+    }
   } in
   assert_equal ast expected
 
 let parse_stmt_list _test_ctxt =
   let code = "@VB\n @@\n sub main()\n if true then\n end if\n while (true)\n end while\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main", [`IfStmt(`TrueExpr, `BlockStmt []); `WhileStmt(`TrueExpr, `BlockStmt [])]);
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `IfStmt({
+            a = `TrueExpr;
+            lnum = 1;
+          }, `BlockStmt [], 1);
+        `WhileStmt({
+            a = `TrueExpr;
+            lnum = 3;
+          }, `BlockStmt [], 3);
+      ];
+      start_lnum = 0;
+      end_lnum = 5;
+    }
   } in
   assert_equal ast expected
 
 let parse_negative_int _test_ctxt =
   let code = "@VB\n @@\n sub main()\n foo(-1)\n end sub" in
   let ast = parse_string code in
-  let expected : AST.translation_unit = {
+  let expected = {
     extension_fxns = [];
-    main_fxn = ("main"), [
-        `ExprStmt(`FxnAppExpr(`IdExpr("foo"), [`IntExpr(-1)]))
+    main_fxn = {
+      id = "main";
+      stmts = [
+        `ExprStmt({
+            a = `FxnAppExpr({
+                a = `IdExpr("foo");
+                lnum = 1;
+              }, [{
+                a = `IntExpr(-1);
+                lnum = 1;
+              }]);
+            lnum = 1;
+          })
       ];
+      start_lnum = 0;
+      end_lnum = 2;
+    }
   } in
   assert_equal ast expected
 
