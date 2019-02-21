2f1567f25454ab80eef2a8d8600eba5ab231d643
Ben Konz <bkonz@unomaha.edu>
2019-02-21 12:19:42 -0600

    Fixed JerooCompilerUtils converter

--- src/compiler/JerooCompilerUtils.ml
+++ src/compiler/JerooCompilerUtils.ml
@@ -27,40 +27,40 @@ let int_of_compass_direction direction = match direction with
 let json_of_bytecode bytecode =
   bytecode
   |> Seq.map (fun code -> match code with
-      | Bytecode.JUMP n -> new_instruction "JUMP" n 0 0 0 0 0
-      | Bytecode.JUMP_LBL _ -> new_instruction "JUMP" (-1) 0 0 0 0 0
-      | Bytecode.BZ n -> new_instruction "BZ" n 0 0 0 0 0
-      | Bytecode.BZ_LBL _ -> new_instruction "BZ" (-1) 0 0 0 0 0
-      | Bytecode.LABEL _ -> new_instruction "JUMP" (-1) 0 0 0 0 0
-      | Bytecode.CALLBK -> new_instruction "CALLBK" 0 0 0 0 0 0
-      | Bytecode.RETR -> new_instruction "RETR" 0 0 0 0 0 0
-      | Bytecode.CSR n -> new_instruction "CSR" n 0 0 0 0 0
-      | Bytecode.NEW (id, x, y, num_flowers, direction) ->
-        new_instruction "NEW" id x y num_flowers (int_of_compass_direction direction) 0
-      | Bytecode.TURN direction ->
-        new_instruction "TURN" (int_of_relative_direction direction) 0 0 0 0 0
-      | Bytecode.HOP n -> new_instruction "HOP" n 0 0 0 0 0
-      | Bytecode.TOSS -> new_instruction "TOSS" 0 0 0 0 0 0
-      | Bytecode.PLANT -> new_instruction "PLANT" 0 0 0 0 0 0
-      | Bytecode.GIVE direction ->
-        new_instruction "GIVE" (int_of_relative_direction direction) 0 0 0 0 0
-      | Bytecode.PICK -> new_instruction "PICK" 0 0 0 0 0 0
-      | Bytecode.TRUE -> new_instruction "TRUE" 0 0 0 0 0 0
-      | Bytecode.FALSE -> new_instruction "FALSE" 0 0 0 0 0 0
-      | Bytecode.HASFLWR -> new_instruction "HASFLWR" 0 0 0 0 0 0
-      | Bytecode.ISNET direction ->
-        new_instruction "ISNET" (int_of_relative_direction direction) 0 0 0 0 0
-      | Bytecode.ISWATER direction ->
-        new_instruction "ISWATER" (int_of_relative_direction direction) 0 0 0 0 0
-      | Bytecode.ISJEROO direction ->
-        new_instruction "ISJEROO" (int_of_relative_direction direction) 0 0 0 0 0
-      | Bytecode.ISFLWR direction ->
-        new_instruction "ISFLWR" (int_of_relative_direction direction) 0 0 0 0 0
-      | Bytecode.FACING direction ->
-        new_instruction "FACING" (int_of_compass_direction direction) 0 0 0 0 0
-      | Bytecode.AND -> new_instruction "AND" 0 0 0 0 0 0
-      | Bytecode.OR -> new_instruction "OR" 0 0 0 0 0 0
-      | Bytecode.NOT -> new_instruction "NOT" 0 0 0 0 0 0
+      | Bytecode.JUMP (n, lnum) -> new_instruction "JUMP" n 0 0 0 0 lnum
+      | Bytecode.JUMP_LBL (_, lnum) -> new_instruction "JUMP" (-1) 0 0 0 0 lnum
+      | Bytecode.BZ (n, lnum) -> new_instruction "BZ" n 0 0 0 0 lnum
+      | Bytecode.BZ_LBL (_, lnum) -> new_instruction "BZ" (-1) 0 0 0 0 lnum
+      | Bytecode.LABEL (_, lnum) -> new_instruction "JUMP" (-1) 0 0 0 0 lnum
+      | Bytecode.CALLBK lnum -> new_instruction "CALLBK" 0 0 0 0 0 lnum
+      | Bytecode.RETR lnum -> new_instruction "RETR" 0 0 0 0 0 lnum
+      | Bytecode.CSR (n, lnum) -> new_instruction "CSR" n 0 0 0 0 lnum
+      | Bytecode.NEW (id, x, y, num_flowers, direction, lnum) ->
+        new_instruction "NEW" id x y num_flowers (int_of_compass_direction direction) lnum
+      | Bytecode.TURN (direction, lnum) ->
+        new_instruction "TURN" (int_of_relative_direction direction) 0 0 0 0 lnum
+      | Bytecode.HOP (n, lnum) -> new_instruction "HOP" n 0 0 0 0 lnum
+      | Bytecode.TOSS lnum -> new_instruction "TOSS" 0 0 0 0 0 lnum
+      | Bytecode.PLANT lnum -> new_instruction "PLANT" 0 0 0 0 0 lnum
+      | Bytecode.GIVE (direction, lnum) ->
+        new_instruction "GIVE" (int_of_relative_direction direction) 0 0 0 0 lnum
+      | Bytecode.PICK lnum -> new_instruction "PICK" 0 0 0 0 0 lnum
+      | Bytecode.TRUE lnum -> new_instruction "TRUE" 0 0 0 0 0 lnum
+      | Bytecode.FALSE lnum -> new_instruction "FALSE" 0 0 0 0 0 lnum
+      | Bytecode.HASFLWR lnum -> new_instruction "HASFLWR" 0 0 0 0 0 lnum
+      | Bytecode.ISNET (direction, lnum) ->
+        new_instruction "ISNET" (int_of_relative_direction direction) 0 0 0 0 lnum
+      | Bytecode.ISWATER (direction, lnum) ->
+        new_instruction "ISWATER" (int_of_relative_direction direction) 0 0 0 0 lnum
+      | Bytecode.ISJEROO (direction, lnum) ->
+        new_instruction "ISJEROO" (int_of_relative_direction direction) 0 0 0 0 lnum
+      | Bytecode.ISFLWR (direction, lnum) ->
+        new_instruction "ISFLWR" (int_of_relative_direction direction) 0 0 0 0 lnum
+      | Bytecode.FACING (direction, lnum) ->
+        new_instruction "FACING" (int_of_compass_direction direction) 0 0 0 0 lnum
+      | Bytecode.AND lnum -> new_instruction "AND" 0 0 0 0 0 lnum
+      | Bytecode.OR lnum -> new_instruction "OR" 0 0 0 0 0 lnum
+      | Bytecode.NOT lnum -> new_instruction "NOT" 0 0 0 0 0 lnum
     )
   |> Array.of_seq
   |> Js.array
