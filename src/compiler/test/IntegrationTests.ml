e485a2280759d5e702282a6884c7806c8d7f3118
Ben Konz <bkonz@unomaha.edu>
2019-02-21 18:38:27 -0600

    Fixed integration tests

--- src/compiler/test/IntegrationTests.ml
+++ src/compiler/test/IntegrationTests.ml
@@ -5,32 +5,32 @@ let compile_java _test_ctxt =
   let code = "@Java\n\n method foo() { hop(3); }\n@@\n method main() { Jeroo j = new Jeroo(); j.foo(); }" in
   let bytecode = List.of_seq (Compiler.compile code) in
   assert_equal bytecode [
-    Bytecode.JUMP (3, 0);
+    Bytecode.JUMP (3, 1);
     Bytecode.HOP (3, 2);
     Bytecode.RETR 2;
-    Bytecode.NEW (0, 0, 0, 0, Bytecode.North, 0);
-    Bytecode.CSR (0, 0);
-    Bytecode.CALLBK 0;
-    Bytecode.JUMP (1, 0);
-    Bytecode.RETR 0;
+    Bytecode.NEW (0, 0, 0, 0, Bytecode.North, 1);
+    Bytecode.CSR (0, 1);
+    Bytecode.CALLBK 1;
+    Bytecode.JUMP (1, 1);
+    Bytecode.RETR 1;
   ]
 
 let compile_VB _test_ctxt =
   let code = "@VB\n\n sub foo()\n hop(3)\n end sub\n @@\n sub main()\n dim j as Jeroo = new Jeroo()\n j.foo()\n end sub" in
   let bytecode = List.of_seq (Compiler.compile code) in
   assert_equal bytecode [
-    Bytecode.JUMP (3, 0);
+    Bytecode.JUMP (3, 1);
     Bytecode.HOP (3, 3);
     Bytecode.RETR 4;
-    Bytecode.NEW (0, 0, 0, 0, Bytecode.North, 1);
-    Bytecode.CSR (0, 2);
-    Bytecode.CALLBK 2;
-    Bytecode.JUMP (1, 2);
-    Bytecode.RETR 3;
+    Bytecode.NEW (0, 0, 0, 0, Bytecode.North, 2);
+    Bytecode.CSR (0, 3);
+    Bytecode.CALLBK 3;
+    Bytecode.JUMP (1, 3);
+    Bytecode.RETR 4;
   ]
 
 let suite =
   "Integration Tests">::: [
-    (* "Compile Java">:: compile_java;
-     * "Compile VB">:: compile_VB; *)
+    "Compile Java">:: compile_java;
+    "Compile VB">:: compile_VB;
   ]
