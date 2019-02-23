04fc15d147247f3425d3bfcd641cb4a4998d701a
Ben Konz <bkonz@unomaha.edu>
2019-02-23 14:28:34 -0600

    Fixed Compiler utils and CLI to work with pane numbers

--- src/compiler/JerooCompilerUtils.ml
+++ src/compiler/JerooCompilerUtils.ml
@@ -15,40 +15,40 @@ let new_instruction op_init arg1 arg2 arg3 arg4 arg5 arg6 =
 let json_of_bytecode bytecode =
   bytecode
   |> Seq.map (fun code -> match code with
-      | Bytecode.JUMP (n, lnum) -> new_instruction "JUMP" n 0 0 0 0 lnum
-      | Bytecode.JUMP_LBL (_, lnum) -> new_instruction "JUMP" (-1) 0 0 0 0 lnum
-      | Bytecode.BZ (n, lnum) -> new_instruction "BZ" n 0 0 0 0 lnum
-      | Bytecode.BZ_LBL (_, lnum) -> new_instruction "BZ" (-1) 0 0 0 0 lnum
-      | Bytecode.LABEL (_, lnum) -> new_instruction "JUMP" (-1) 0 0 0 0 lnum
-      | Bytecode.CALLBK lnum -> new_instruction "CALLBK" 0 0 0 0 0 lnum
-      | Bytecode.RETR lnum -> new_instruction "RETR" 0 0 0 0 0 lnum
-      | Bytecode.CSR (n, lnum) -> new_instruction "CSR" n 0 0 0 0 lnum
-      | Bytecode.NEW (id, x, y, num_flowers, direction, lnum) ->
-        new_instruction "NEW" id x y num_flowers (Bytecode.int_of_compass_direction direction) lnum
-      | Bytecode.TURN (direction, lnum) ->
-        new_instruction "TURN" (Bytecode.int_of_relative_direction direction) 0 0 0 0 lnum
-      | Bytecode.HOP (n, lnum) -> new_instruction "HOP" n 0 0 0 0 lnum
-      | Bytecode.TOSS lnum -> new_instruction "TOSS" 0 0 0 0 0 lnum
-      | Bytecode.PLANT lnum -> new_instruction "PLANT" 0 0 0 0 0 lnum
-      | Bytecode.GIVE (direction, lnum) ->
-        new_instruction "GIVE" (Bytecode.int_of_relative_direction direction) 0 0 0 0 lnum
-      | Bytecode.PICK lnum -> new_instruction "PICK" 0 0 0 0 0 lnum
-      | Bytecode.TRUE lnum -> new_instruction "TRUE" 0 0 0 0 0 lnum
-      | Bytecode.FALSE lnum -> new_instruction "FALSE" 0 0 0 0 0 lnum
-      | Bytecode.HASFLWR lnum -> new_instruction "HASFLWR" 0 0 0 0 0 lnum
-      | Bytecode.ISNET (direction, lnum) ->
-        new_instruction "ISNET" (Bytecode.int_of_relative_direction direction) 0 0 0 0 lnum
-      | Bytecode.ISWATER (direction, lnum) ->
-        new_instruction "ISWATER" (Bytecode.int_of_relative_direction direction) 0 0 0 0 lnum
-      | Bytecode.ISJEROO (direction, lnum) ->
-        new_instruction "ISJEROO" (Bytecode.int_of_relative_direction direction) 0 0 0 0 lnum
-      | Bytecode.ISFLWR (direction, lnum) ->
-        new_instruction "ISFLWR" (Bytecode.int_of_relative_direction direction) 0 0 0 0 lnum
-      | Bytecode.FACING (direction, lnum) ->
-        new_instruction "FACING" (Bytecode.int_of_compass_direction direction) 0 0 0 0 lnum
-      | Bytecode.AND lnum -> new_instruction "AND" 0 0 0 0 0 lnum
-      | Bytecode.OR lnum -> new_instruction "OR" 0 0 0 0 0 lnum
-      | Bytecode.NOT lnum -> new_instruction "NOT" 0 0 0 0 0 lnum
+      | Bytecode.JUMP (n, pane_num, line_num) -> new_instruction "JUMP" n 0 0 0 pane_num line_num
+      | Bytecode.JUMP_LBL (_, pane_num, line_num) -> new_instruction "JUMP" (-1) 0 0 0 pane_num line_num
+      | Bytecode.BZ (n, pane_num, line_num) -> new_instruction "BZ" n 0 0 0 pane_num line_num
+      | Bytecode.BZ_LBL (_, pane_num, line_num) -> new_instruction "BZ" (-1) 0 0 0 pane_num line_num
+      | Bytecode.LABEL (_, pane_num, line_num) -> new_instruction "JUMP" (-1) 0 0 0 pane_num line_num
+      | Bytecode.CALLBK (pane_num, line_num) -> new_instruction "CALLBK" 0 0 0 0 pane_num line_num
+      | Bytecode.RETR (pane_num, line_num) -> new_instruction "RETR" 0 0 0 0 pane_num line_num
+      | Bytecode.CSR (n, pane_num, line_num) -> new_instruction "CSR" n 0 0 0 pane_num line_num
+      | Bytecode.NEW (id, x, y, num_flowers, direction, line_num) ->
+        new_instruction "NEW" id x y num_flowers (Bytecode.int_of_compass_direction direction) line_num
+      | Bytecode.TURN (direction, pane_num, line_num) ->
+        new_instruction "TURN" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.HOP (n, pane_num, line_num) -> new_instruction "HOP" n 0 0 0 pane_num line_num
+      | Bytecode.TOSS (pane_num, line_num) -> new_instruction "TOSS" 0 0 0 0 pane_num line_num
+      | Bytecode.PLANT (pane_num, line_num) -> new_instruction "PLANT" 0 0 0 0 pane_num line_num
+      | Bytecode.GIVE (direction, pane_num, line_num) ->
+        new_instruction "GIVE" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.PICK (pane_num, line_num) -> new_instruction "PICK" 0 0 0 0 pane_num line_num
+      | Bytecode.TRUE (pane_num, line_num) -> new_instruction "TRUE" 0 0 0 0 pane_num line_num
+      | Bytecode.FALSE (pane_num, line_num) -> new_instruction "FALSE" 0 0 0 0 pane_num line_num
+      | Bytecode.HASFLWR (pane_num, line_num) -> new_instruction "HASFLWR" 0 0 0 0 pane_num line_num
+      | Bytecode.ISNET (direction, pane_num, line_num) ->
+        new_instruction "ISNET" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.ISWATER (direction, pane_num, line_num) ->
+        new_instruction "ISWATER" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.ISJEROO (direction, pane_num, line_num) ->
+        new_instruction "ISJEROO" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.ISFLWR (direction, pane_num, line_num) ->
+        new_instruction "ISFLWR" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.FACING (direction, pane_num, line_num) ->
+        new_instruction "FACING" (Bytecode.int_of_compass_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.AND (pane_num, line_num) -> new_instruction "AND" 0 0 0 0 pane_num line_num
+      | Bytecode.OR (pane_num, line_num) -> new_instruction "OR" 0 0 0 0 pane_num line_num
+      | Bytecode.NOT (pane_num, line_num) -> new_instruction "NOT" 0 0 0 0 pane_num line_num
     )
   |> Array.of_seq
   |> Js.array
--- src/compiler/cli/JerooCompilerCLI.ml
+++ src/compiler/cli/JerooCompilerCLI.ml
@@ -14,39 +14,39 @@ let _ =
 
   bytecode
   |> Seq.map (fun code -> match code with
-      | Bytecode.JUMP (n, line_num) -> str_of_instr "JUMP" n 0 0 0 0 line_num
-      | Bytecode.JUMP_LBL (_, line_num) -> str_of_instr "JUMP" (-1) 0 0 0 0 line_num
-      | Bytecode.BZ (n, line_num) -> str_of_instr "BZ" n 0 0 0 0 line_num
-      | Bytecode.BZ_LBL (_, line_num) -> str_of_instr "BZ" (-1) 0 0 0 0 line_num
-      | Bytecode.LABEL (_, line_num) -> str_of_instr "JUMP" (-1) 0 0 0 0 line_num
-      | Bytecode.CALLBK line_num -> str_of_instr "CALLBK" 0 0 0 0 0 line_num
-      | Bytecode.RETR line_num -> str_of_instr "RETR" 0 0 0 0 0 line_num
-      | Bytecode.CSR (n, line_num) -> str_of_instr "CSR" n 0 0 0 0 line_num
+      | Bytecode.JUMP (n, pane_num, line_num) -> str_of_instr "JUMP" n 0 0 0 pane_num line_num
+      | Bytecode.JUMP_LBL (_, pane_num, line_num) -> str_of_instr "JUMP" (-1) 0 0 0 pane_num line_num
+      | Bytecode.BZ (n, pane_num, line_num) -> str_of_instr "BZ" n 0 0 0 pane_num line_num
+      | Bytecode.BZ_LBL (_, pane_num, line_num) -> str_of_instr "BZ" (-1) 0 0 0 pane_num line_num
+      | Bytecode.LABEL (_, pane_num, line_num) -> str_of_instr "JUMP" (-1) 0 0 0 pane_num line_num
+      | Bytecode.CALLBK (pane_num, line_num) -> str_of_instr "CALLBK" 0 0 0 0 pane_num line_num
+      | Bytecode.RETR (pane_num, line_num) -> str_of_instr "RETR" 0 0 0 0 pane_num line_num
+      | Bytecode.CSR (n, pane_num, line_num) -> str_of_instr "CSR" n 0 0 0 pane_num line_num
       | Bytecode.NEW (id, x, y, num_flowers, direction, line_num) ->
         str_of_instr "NEW" id x y num_flowers (Bytecode.int_of_compass_direction direction) line_num
-      | Bytecode.TURN (direction, line_num) ->
-        str_of_instr "TURN" (Bytecode.int_of_relative_direction direction) 0 0 0 0 line_num
-      | Bytecode.HOP (n, line_num) -> str_of_instr "HOP" n 0 0 0 0 line_num
-      | Bytecode.TOSS line_num -> str_of_instr "TOSS" 0 0 0 0 0 line_num
-      | Bytecode.PLANT line_num -> str_of_instr "PLANT" 0 0 0 0 0 line_num
-      | Bytecode.GIVE (direction, line_num) ->
-        str_of_instr "GIVE" (Bytecode.int_of_relative_direction direction) 0 0 0 0 line_num
-      | Bytecode.PICK line_num -> str_of_instr "PICK" 0 0 0 0 0 line_num
-      | Bytecode.TRUE line_num -> str_of_instr "TRUE" 0 0 0 0 0 line_num
-      | Bytecode.FALSE line_num -> str_of_instr "FALSE" 0 0 0 0 0 line_num
-      | Bytecode.HASFLWR line_num -> str_of_instr "HASFLWR" 0 0 0 0 0 line_num
-      | Bytecode.ISNET (direction, line_num) ->
-        str_of_instr "ISNET" (Bytecode.int_of_relative_direction direction) 0 0 0 0 line_num
-      | Bytecode.ISWATER (direction, line_num) ->
-        str_of_instr "ISWATER" (Bytecode.int_of_relative_direction direction) 0 0 0 0 line_num
-      | Bytecode.ISJEROO (direction, line_num) ->
-        str_of_instr "ISJEROO" (Bytecode.int_of_relative_direction direction) 0 0 0 0 line_num
-      | Bytecode.ISFLWR (direction, line_num) ->
-        str_of_instr "ISFLWR" (Bytecode.int_of_relative_direction direction) 0 0 0 0 line_num
-      | Bytecode.FACING (direction, line_num) ->
-        str_of_instr "FACING" (Bytecode.int_of_compass_direction direction) 0 0 0 0 line_num
-      | Bytecode.AND line_num -> str_of_instr "AND" 0 0 0 0 0 line_num
-      | Bytecode.OR line_num -> str_of_instr "OR" 0 0 0 0 0 line_num
-      | Bytecode.NOT line_num -> str_of_instr "NOT" 0 0 0 0 0 line_num
+      | Bytecode.TURN (direction, pane_num, line_num) ->
+        str_of_instr "TURN" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.HOP (n, pane_num, line_num) -> str_of_instr "HOP" n 0 0 0 pane_num line_num
+      | Bytecode.TOSS (pane_num, line_num) -> str_of_instr "TOSS" 0 0 0 0 pane_num line_num
+      | Bytecode.PLANT (pane_num, line_num) -> str_of_instr "PLANT" 0 0 0 0 pane_num line_num
+      | Bytecode.GIVE (direction, pane_num, line_num) ->
+        str_of_instr "GIVE" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.PICK (pane_num, line_num) -> str_of_instr "PICK" 0 0 0 0 pane_num line_num
+      | Bytecode.TRUE (pane_num, line_num) -> str_of_instr "TRUE" 0 0 0 0 pane_num line_num
+      | Bytecode.FALSE (pane_num, line_num) -> str_of_instr "FALSE" 0 0 0 0 pane_num line_num
+      | Bytecode.HASFLWR (pane_num, line_num) -> str_of_instr "HASFLWR" 0 0 0 0 pane_num line_num
+      | Bytecode.ISNET (direction, pane_num, line_num) ->
+        str_of_instr "ISNET" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.ISWATER (direction, pane_num, line_num) ->
+        str_of_instr "ISWATER" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.ISJEROO (direction, pane_num, line_num) ->
+        str_of_instr "ISJEROO" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.ISFLWR (direction, pane_num, line_num) ->
+        str_of_instr "ISFLWR" (Bytecode.int_of_relative_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.FACING (direction, pane_num, line_num) ->
+        str_of_instr "FACING" (Bytecode.int_of_compass_direction direction) 0 0 0 pane_num line_num
+      | Bytecode.AND (pane_num, line_num) -> str_of_instr "AND" 0 0 0 0 pane_num line_num
+      | Bytecode.OR (pane_num, line_num) -> str_of_instr "OR" 0 0 0 0 pane_num line_num
+      | Bytecode.NOT (pane_num, line_num) -> str_of_instr "NOT" 0 0 0 0 pane_num line_num
     )
   |> Seq.iter (fun code_str -> print_endline code_str)
