507042864c1b7b8f292793c6a691735b19e99908
Ben Konz <bkonz@unomaha.edu>
2019-02-22 22:50:13 -0600

    Added comments to codegen module

--- src/compiler/lib/Codegen.mli
+++ src/compiler/lib/Codegen.mli
@@ -1,3 +1,4 @@
-exception SemanticException of string
+(** Module to compiler AST's to a sequence of bytecode instruction *)
 
+(** compile a syntax tree into a sequence of bytecode instructions *)
 val codegen : AST.translation_unit -> Bytecode.bytecode Seq.t
