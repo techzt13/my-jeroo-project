a3b9fe51fbdacab28c11a7352a8fc7ce54f50e82
Ben Konz <bkonz@unomaha.edu>
2019-03-04 21:11:47 -0600

    Fixed tutorial routes

--- src/app/help/help-routing.module.ts
+++ src/app/help/help-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { VBHelpComponent } from './vbhelp/vbhelp.component';
 import { PythonHelpComponent } from './python-help/python-help.component';
 import { JavaHelpComponent } from './java-help/java-help.component';
+import { HelpTutorialJavaComponent } from './tutorial/help-tutorial-java/help-tutorial-java.component';
+import { HelpTutorialVbComponent } from './tutorial/help-tutorial-vb/help-tutorial-vb.component';
+import { HelpTutorialPythonComponent } from './tutorial/help-tutorial-python/help-tutorial-python.component';
 
 const routes: Routes = [
     {
@@ -16,6 +19,18 @@ const routes: Routes = [
     {
         path: 'python',
         component: PythonHelpComponent
+    },
+    {
+        path: 'java/tutorial',
+        component: HelpTutorialJavaComponent
+    },
+    {
+        path: 'vb/tutorial',
+        component: HelpTutorialVbComponent
+    },
+    {
+        path: 'python/tutorial',
+        component: HelpTutorialPythonComponent
     }
 ];
 
