925bb373be27f4c91e654fcab2cb55cb1d17f9a1
Ben Konz <bkonz@unomaha.edu>
2019-03-16 11:46:33 -0500

    Added jeroo-java mode to CodeMirrorjk

---
+++ src/app/text-editor/javaMode.ts
@@ -0,0 +1,52 @@
+export const javaMode: any = {
+    start: [
+        {
+            regex: /method|while|if|else/,
+            token: 'keyword'
+        },
+        {
+            regex: /true|false|new|NORTH|SOUTH|EAST|WEST|AHEAD|LEFT|RIGHT|HERE/,
+            token: 'atom'
+        },
+        {
+            regex: /[+-]?[0-9]+/,
+            token: 'number'
+        },
+        {
+            regex: /[-+=<>&|!]+/,
+            token: 'operator'
+        },
+        {
+            regex: /[\{\(]/,
+            indent: true
+        },
+        {
+            regex: /[\}\)]/,
+            dedent: true
+        },
+        {
+            regex: /\/\/.*/,
+            token: 'comment'
+        },
+        {
+            regex: /\/\*/,
+            token: 'comment',
+            next: 'comment'
+        }
+    ],
+    comment: [
+        {
+            regex: /.*?\*\//,
+            token: 'comment',
+            next: 'start'
+        },
+        {
+            regex: /.*/,
+            token: 'comment'
+        }
+    ],
+    meta: {
+        dontIndentStates: ['comment'],
+        lineComment: '//'
+    }
+};
--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,25 +1,34 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import 'codemirror/lib/codemirror';
-import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/mode/simple';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
 import * as CodeMirror from 'codemirror';
+import { javaMode } from './javaMode';
 
 @Component({
     selector: 'app-text-editor',
     templateUrl: './text-editor.component.html'
 })
 export class TextEditorComponent implements AfterViewInit {
-    @ViewChild('editorTextarea') editorTextarea: ElementRef;
+    @ViewChild('editorTextarea') editorTextArea: ElementRef;
     private editor: CodeMirror.Editor = null;
 
     constructor() { }
 
     ngAfterViewInit() {
-        const editorTextArea = this.editorTextarea.nativeElement as HTMLTextAreaElement;
+        const editorTextArea = this.editorTextArea.nativeElement as HTMLTextAreaElement;
+        // for some reason defineSimpleMode isn't part of the CodeMirror type
+        (CodeMirror as any).defineSimpleMode('jeroo-java', javaMode);
         this.editor = CodeMirror.fromTextArea(editorTextArea, {
-            mode: 'javascript',
+            mode: 'jeroo-java',
             theme: 'default',
-            lineNumbers: true
-        });
+            lineNumbers: true,
+            // not technically part of the type, but is allowed in CodeMirror
+            // because of the addons that are imported
+            matchBrackets: true,
+            autoCloseBrackets: true
+        } as CodeMirror.EditorConfiguration);
         this.editor.refresh();
     }
 }
