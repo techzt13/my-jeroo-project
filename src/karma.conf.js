0a36a2885df86404cf645271797836428a6d9664
konz <benjamin.konz@impact.com>
2019-05-20 19:48:58 -0400

    made firefox default browser for tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
-            require('karma-chrome-launcher'),
+            require('karma-firefox-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
@@ -27,9 +27,9 @@ module.exports = function (config) {
         autoWatch: true,
         browsers: ['Chrome'],
         customLaunchers: {
-            ChromeHeadlessCI: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox']
+            FirefoxHeadlessCI: {
+                base: 'Firefox',
+                flags: ['-headless']
             }
         },
         singleRun: false
