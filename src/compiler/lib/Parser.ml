4c4cd533ff9b25ba4c5f2182c392356c8c0b73d7
konz <benjamin.konz@impact.com>
2019-08-26 16:22:31 -0500

    removed preview of user code for now

--- src/compiler/lib/Parser.ml
+++ src/compiler/lib/Parser.ml
@@ -24,7 +24,7 @@ let make_main_fxn = function
 
 let parse_java extensions_code main_code =
   let parse code pane =
-    let rec loop lexbuf checkpoint pane last_token_opt =
+    let rec loop lexbuf checkpoint pane =
       try
         match checkpoint with
         | JavaParser.MenhirInterpreter.InputNeeded _ ->
@@ -32,24 +32,15 @@ let parse_java extensions_code main_code =
           let startp = lexbuf.lex_start_p in
           let endp = lexbuf.lex_curr_p in
           let checkpoint = JavaParser.MenhirInterpreter.offer checkpoint (token, startp, endp) in
-          loop lexbuf checkpoint pane (Some token)
+          loop lexbuf checkpoint pane
         | JavaParser.MenhirInterpreter.Shifting _ | JavaParser.MenhirInterpreter.AboutToReduce _ ->
           let checkpoint = JavaParser.MenhirInterpreter.resume checkpoint in
-          loop lexbuf checkpoint pane last_token_opt
+          loop lexbuf checkpoint pane
         | JavaParser.MenhirInterpreter.HandlingError env ->
           (* make the error message prefix, ex: "expected `;`" *)
           let error_state = JavaParser.MenhirInterpreter.current_state_number env in
-          let message_prefix = JavaMessages.message error_state |> String.trim in
+          let message = JavaMessages.message error_state in
 
-          (* make the error message suffix, ex: ", found `)`" *)
-          let message_suffix = match last_token_opt with
-            | Some token when token <> JavaParser.EOF ->
-              Printf.sprintf ", found `%s`"
-                (JavaTokenMapper.map_token token)
-            | _ -> ""
-          in
-
-          let message = message_prefix ^ message_suffix ^ "\n" in
           raise (Exceptions.CompileException {
               pos = {
                 lnum = LexingUtils.get_lnum lexbuf;
@@ -82,7 +73,7 @@ let parse_java extensions_code main_code =
           })
     in
     let lexbuf = Lexing.from_string code in
-    loop lexbuf (JavaParser.Incremental.translation_unit lexbuf.lex_curr_p) pane None
+    loop lexbuf (JavaParser.Incremental.translation_unit lexbuf.lex_curr_p) pane
   in
   let extension_fxns = parse extensions_code Pane.Extensions in
   let main_fxns = parse main_code Pane.Main in
@@ -94,7 +85,7 @@ let parse_java extensions_code main_code =
 
 let parse_vb extensions_code main_code =
   let parse code pane =
-    let rec loop lexbuf state checkpoint pane last_token_opt =
+    let rec loop lexbuf state checkpoint pane =
       try
         match checkpoint with
         | VBParser.MenhirInterpreter.InputNeeded _ ->
@@ -102,24 +93,15 @@ let parse_vb extensions_code main_code =
           let startp = lexbuf.lex_start_p in
           let endp = lexbuf.lex_curr_p in
           let checkpoint = VBParser.MenhirInterpreter.offer checkpoint (token, startp, endp) in
-          loop lexbuf state checkpoint pane (Some token)
+          loop lexbuf state checkpoint pane
         | VBParser.MenhirInterpreter.Shifting _ | VBParser.MenhirInterpreter.AboutToReduce _ ->
           let checkpoint = VBParser.MenhirInterpreter.resume checkpoint in
-          loop lexbuf state checkpoint pane last_token_opt
+          loop lexbuf state checkpoint pane
         | VBParser.MenhirInterpreter.HandlingError env ->
           (* make the error message prefix, ex: "expected new line" *)
           let error_state = VBParser.MenhirInterpreter.current_state_number env in
-          let message_prefix = VBMessages.message error_state |> String.trim in
+          let message = VBMessages.message error_state in
 
-          (* make the message suffix, ex: ", found `)`" *)
-          let message_suffix = match last_token_opt with
-            | Some last_token when last_token <> VBParser.EOF ->
-              Printf.sprintf ", found `%s`"
-                (VBTokenMapper.map_token last_token)
-            | _ -> ""
-          in
-
-          let message = message_prefix ^ message_suffix ^ "\n" in
           raise (Exceptions.CompileException {
               pos = {
                 lnum = LexingUtils.get_lnum lexbuf;
@@ -150,7 +132,7 @@ let parse_vb extensions_code main_code =
         })
     in
     let lexbuf = Lexing.from_string code in
-    loop lexbuf (VBLexerState.create ()) (VBParser.Incremental.translation_unit lexbuf.lex_curr_p) pane None
+    loop lexbuf (VBLexerState.create ()) (VBParser.Incremental.translation_unit lexbuf.lex_curr_p) pane
   in
   let extension_fxns = parse extensions_code Pane.Extensions in
   let main_fxns = parse main_code Pane.Main in
@@ -161,7 +143,7 @@ let parse_vb extensions_code main_code =
   }
 
 let parse_python extensions_code main_code =
-  let rec loop code lexbuf state checkpoint pane last_token_opt =
+  let rec loop code lexbuf state checkpoint pane =
     try
       match checkpoint with
       | PythonParser.MenhirInterpreter.InputNeeded _ ->
@@ -169,23 +151,15 @@ let parse_python extensions_code main_code =
         let startp = lexbuf.lex_start_p in
         let endp = lexbuf.lex_curr_p in
         let checkpoint = PythonParser.MenhirInterpreter.offer checkpoint (token, startp, endp) in
-        loop code lexbuf state checkpoint pane (Some token)
+        loop code lexbuf state checkpoint pane
       | PythonParser.MenhirInterpreter.Shifting _ | PythonParser.MenhirInterpreter.AboutToReduce _ ->
         let checkpoint = PythonParser.MenhirInterpreter.resume checkpoint in
-        loop code lexbuf state checkpoint pane last_token_opt
+        loop code lexbuf state checkpoint pane
       | PythonParser.MenhirInterpreter.HandlingError env ->
         (* make the error message prefix, ex: "expected `:`" *)
         let error_state = PythonParser.MenhirInterpreter.current_state_number env in
-        let message_prefix = PythonMessages.message error_state |> String.trim in
+        let message = PythonMessages.message error_state in
 
-        (* make the error message suffix, ex: ", found `)`" *)
-        let message_suffix = match last_token_opt with
-          | Some last_token when last_token <> PythonParser.EOF ->
-            Printf.sprintf ", found `%s`" (PythonTokenMapper.map_token last_token)
-          | _ -> ""
-        in
-
-        let message = message_prefix ^ message_suffix ^ "\n" in
         raise (Exceptions.CompileException {
             pos = {
               lnum = LexingUtils.get_lnum lexbuf;
@@ -223,7 +197,6 @@ let parse_python extensions_code main_code =
       (PythonLexerState.create ())
       (PythonParser.Incremental.fxns extensions_code_lexbuf.lex_curr_p)
       Pane.Extensions
-      None
   in
   let main_code_lexbuf = Lexing.from_string main_code in
   let main_fxn = loop
@@ -232,7 +205,6 @@ let parse_python extensions_code main_code =
       (PythonLexerState.create ())
       (PythonParser.Incremental.main_fxn main_code_lexbuf.lex_curr_p)
       Pane.Main
-      None
   in
   {
     extension_fxns;
