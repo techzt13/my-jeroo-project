4a0006b6e81bf82775ae4a1d5f442874dccc1ff5
Zack Tudor <zackt.atp@gmail.com>
2026-01-21 23:43:48 +0000

    Add help components for variables in Java, Python, and VB

--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -25,18 +25,21 @@ import { HelpControlStructJavaComponent } from './java-help/help-control-struct-
 import { HelpGeneralJavaComponent } from './java-help/help-general-java/help-general-java.component';
 import { HelpInstantiationJavaComponent } from './java-help/help-instantiation-java/help-instantiation-java.component';
 import { HelpMethodJavaComponent } from './java-help/help-method-java/help-method-java.component';
+import { HelpVariablesJavaComponent } from './java-help/help-variables-java/help-variables-java.component';
 import { PythonHelpComponent } from './python-help/python-help.component';
 import { HelpConditionPythonComponent } from './python-help/help-condition-python/help-condition-python.component';
 import { HelpControlStructPythonComponent } from './python-help/help-control-struct-python/help-control-struct-python.component';
 import { HelpGeneralPythonComponent } from './python-help/help-general-python/help-general-python.component';
 import { HelpInstantiationPythonComponent } from './python-help/help-instantiation-python/help-instantiation-python.component';
 import { HelpMethodPythonComponent } from './python-help/help-method-python/help-method-python.component';
+import { HelpVariablesPythonComponent } from './python-help/help-variables-python/help-variables-python.component';
 import { VBHelpComponent } from './vbhelp/vbhelp.component';
 import { HelpConditionVBComponent } from './vbhelp/help-condition-vb/help-condition-vb.component';
 import { HelpControlStructVBComponent } from './vbhelp/help-control-struct-vb/help-control-struct-vb.component';
 import { HelpGeneralVbComponent } from './vbhelp/help-general-vb/help-general-vb.component';
 import { HelpInstantiationVbComponent } from './vbhelp/help-instantiation-vb/help-instantiation-vb.component';
 import { HelpMethodVbComponent } from './vbhelp/help-method-vb/help-method-vb.component';
+import { HelpVariablesVbComponent } from './vbhelp/help-variables-vb/help-variables-vb.component';
 import { HelpActionComponent } from './general/help-action/help-action.component';
 import { HelpBooleanComponent } from './general/help-boolean/help-boolean.component';
 import { HelpCompassComponent } from './general/help-compass/help-compass.component';
@@ -56,18 +59,21 @@ import { CommonModule } from '@angular/common';
     HelpGeneralJavaComponent,
     HelpInstantiationJavaComponent,
     HelpMethodJavaComponent,
+    HelpVariablesJavaComponent,
     PythonHelpComponent,
     HelpConditionPythonComponent,
     HelpControlStructPythonComponent,
     HelpGeneralPythonComponent,
     HelpInstantiationPythonComponent,
     HelpMethodPythonComponent,
+    HelpVariablesPythonComponent,
     VBHelpComponent,
     HelpConditionVBComponent,
     HelpControlStructVBComponent,
     HelpGeneralVbComponent,
     HelpInstantiationVbComponent,
     HelpMethodVbComponent,
+    HelpVariablesVbComponent,
     HelpActionComponent,
     HelpBooleanComponent,
     HelpCompassComponent,
@@ -89,18 +95,21 @@ import { CommonModule } from '@angular/common';
     HelpGeneralJavaComponent,
     HelpInstantiationJavaComponent,
     HelpMethodJavaComponent,
+    HelpVariablesJavaComponent,
     PythonHelpComponent,
     HelpConditionPythonComponent,
     HelpControlStructPythonComponent,
     HelpGeneralPythonComponent,
     HelpInstantiationPythonComponent,
     HelpMethodPythonComponent,
+    HelpVariablesPythonComponent,
     VBHelpComponent,
     HelpConditionVBComponent,
     HelpControlStructVBComponent,
     HelpGeneralVbComponent,
     HelpInstantiationVbComponent,
     HelpMethodVbComponent,
+    HelpVariablesVbComponent,
     HelpActionComponent,
     HelpBooleanComponent,
     HelpCompassComponent,
